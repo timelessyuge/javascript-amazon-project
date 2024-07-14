1544a92987f3f63630dbb304fe261de4588c7378
timelessyuge <timelessyuge@gmail.com>
2024-07-14 10:50:25 +0800

    use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,3 @@
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127
-    },
-    priceCents: 2095,
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56
-    },
-    priceCents: 799,
-  },
-]
-
 function generateHtmlTemplate(image, name, stars, count, priceCents) {
   htmlTemplate = `<div class="product-container">
           <div class="product-image-container">
@@ -77,14 +47,25 @@ function generateHtmlTemplate(image, name, stars, count, priceCents) {
             Add to Cart
           </button>
         </div>`;
+
   return htmlTemplate;
+
 }
 
 
 let productsHTML = '';
+
 products.forEach(product => {
-  htmlTemplate = generateHtmlTemplate(product.image, product.name, product.rating.stars, product.rating.count, product.priceCents);
+
+  htmlTemplate = generateHtmlTemplate(
+    product.image, 
+    product.name, 
+    product.rating.stars, 
+    product.rating.count, 
+    product.priceCents);
+
   productsHTML += htmlTemplate;
+
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
