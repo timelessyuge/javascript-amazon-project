8ce0c0118e1f8e8e9fd3aacc045942f5575a8ed3
timelessyuge <timelessyuge@gmail.com>
2024-07-14 14:14:21 +0800

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -67,12 +67,10 @@ products.forEach(product => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-
-
 document.querySelectorAll('.js-add-to-cart')
   .forEach(button => {
     button.addEventListener('click', () => {
-      
+
       const productId = button.dataset.productId;
 
       let hasItem = false;
@@ -90,10 +88,22 @@ document.querySelectorAll('.js-add-to-cart')
           quantity:1,
         });
       }
-      // console.log(button.dataset);
+
+
+      let cartQuantity = 0;
+
+      cart.forEach(item => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+      
       console.log(cart);
     });
   });
+
+
+
 
 
 
